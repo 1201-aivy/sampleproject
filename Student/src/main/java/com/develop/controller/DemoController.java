c2e048c17ce894493c25833d3904f98b42fb08a0
1201-aivy <aivykakkassery@gmail.com>
2019-10-21 18:29:35 +0530

    commiting changes in democontroller

--- Student/src/main/java/com/develop/controller/DemoController.java
+++ Student/src/main/java/com/develop/controller/DemoController.java
@@ -3,7 +3,7 @@ package com.develop.controller;
 public class DemoController {
 
 	public static void main(String[] args) {
-		System.out.println("Hi Students");
+		System.out.println("Hi Student");
 
 	}
 
